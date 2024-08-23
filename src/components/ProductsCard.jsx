0cd17410421ac9f94af9aed290630f37c997d7ab
yongqi8899 <1243994415@qq.com>
2024-08-23 09:34:49 +0200

    change color

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,7 +1,7 @@
 import Img from "@/components/Img";
 const ProductsCard = ({ src, title, children }) => {
   return (
-    <div className="card glass w-96 hover:drop-shadow-[0_0_10px_#1E2CBF]">
+    <div className="card glass w-96 hover:drop-shadow-[0_0_5px_#37cdbe]">
       <figure>
         <Img src={src} alt={title} />
       </figure>
