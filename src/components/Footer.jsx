6c6eb6b98d8556feae11695790e775d89db16dd8
yongqi8899 <1243994415@qq.com>
2024-08-23 16:59:28 +0200

    change theme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 p-4 footer footer-center font-redressed">
+    <footer className="fixed bottom-0 p-4 footer footer-center font-redressed bg-neutral">
       <aside>
         <p>Copyright © Yongqi Zhang</p>
       </aside>
