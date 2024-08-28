9e97c64668e7a17dc353fe1e173a2d6305041ca6
yongqi8899 <1243994415@qq.com>
2024-08-28 12:01:27 +0200

    add user layout

--- src/pages/Me.jsx
+++ src/pages/Me.jsx
@@ -5,7 +5,24 @@ import { me } from "@/data/auth/index.js";
 import { useAuth } from "@/context/index.js";
 
 export default function Me() {
+  const { user } = useAuth();
+  console.log(user);
   return (
-    <div>Me</div>
+    user&&(<div className="card card-side bg-neutral shadow-xl max-w-xl m-auto">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          alt="Movie"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{user.userName}</h2>
+        <p>{user.email}</p>
+        <p>{user.role}</p>
+        {/* <div className="card-actions justify-end">
+          <button className="btn btn-primary">Edit</button>
+        </div> */}
+      </div>
+    </div>)
   );
 }
