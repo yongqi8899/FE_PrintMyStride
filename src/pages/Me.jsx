c0d9475069a7d341528fe5d097dfcfd8337a4cf2
yongqi8899 <1243994415@qq.com>
2024-09-02 08:54:23 +0200

    fix me

--- src/pages/Me.jsx
+++ src/pages/Me.jsx
@@ -3,21 +3,23 @@ import { useAuth } from "@/context/index.js";
 export default function Me() {
   const { user } = useAuth();
   return (
-    user&&(<div className="card card-side bg-neutral shadow-xl max-w-xl m-auto">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          alt="Movie"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{user.userName}</h2>
-        <p>{user.email}</p>
-        <p>{user.role}</p>
-        {/* <div className="card-actions justify-end">
+    user && (
+      <div className="card card-side bg-neutral shadow-xl max-w-xl m-auto">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{user.userName}</h2>
+          <p>{user.email}</p>
+          <p>{user.role}</p>
+          {/* <div className="card-actions justify-end">
           <button className="btn btn-primary">Edit</button>
         </div> */}
+        </div>
       </div>
-    </div>)
+    )
   );
 }
