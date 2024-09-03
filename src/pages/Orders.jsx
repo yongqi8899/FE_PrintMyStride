4c4b5eedf55642581f0a37beb9e39675827c3a3f
yongqi8899 <1243994415@qq.com>
2024-09-03 15:23:13 +0200

    add alert

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { useAuth } from "@/context/index.js";
 import { formatDate } from "@/utils/index.js";
+import Alert from "@/components/Alert.jsx";
 
 export default function Orders() {
   const orders = useLoaderData();
