271834e2ef8bfc07eb436c92180a44ee5efddd79
yongqi8899 <1243994415@qq.com>
2024-09-04 11:36:20 +0200

    fix deploy

--- dist/assets/ErrorPage-CvFyn9Lf.js
+++
@@ -1,1 +0,0 @@
-import{d as s,j as e}from"./index-BlMw5Gxn.js";const a=()=>{const r=s();return e.jsxs("div",{className:"  flex flex-col m-10 gap-10",children:[e.jsx("h1",{className:"text-3xl font-bold text-red-400",children:"Error Page"}),e.jsx("p",{className:"",children:e.jsx("i",{children:r.statusText||r.message})})]})};export{a as default};
